0634d1bf6da793c0f99799e602874f736e2ee223
Edward Gomez <edward@gomez.me.uk>
2024-10-23 15:54:28 +0100

    Simplify publish config

--- publishconf.py
+++ publishconf.py
@@ -8,12 +8,12 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://www.zemogle.net/robotic-telescopes/'
-RELATIVE_URLS = False
+# RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+# DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
